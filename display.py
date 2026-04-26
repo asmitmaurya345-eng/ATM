e3b1cff9d12d2dab6c6a3d826c91d8aa997b9ee7
asmitmaurya345-eng <asmitmaurya345@gmail.com>
2026-04-26 13:50:22 +0530

    alpha 0.7

--- display.py
+++ display.py
@@ -3,8 +3,8 @@ class Account():
     def __init__(self,acc_no):
         with open ("database.json","r") as database
         self.data=json.load(database)
-        if self.acc_no in data:
-            per_info=data[acc_no]
+        if self.acc_no in self.data:
+            per_info=self.data[acc_no]
             print("Acount number;",self.acc_no)
             print("Acount Holder Name;",per_info["name"])
             print("Balance:",per_info["balance"])
--- statement.py
+++ statement.py
@@ -1,5 +1,9 @@
 import json
 class Account():
     def __init__(self):
-        with open ("database.json","a") as database
-        self.
+        while True:
+            self.acc_no=input("Enter yout account number:")
+            with open ("database.json","a") as database
+            self.data=json.load(database)
+            self.per_info=self.data[self.acc_no]
+            
